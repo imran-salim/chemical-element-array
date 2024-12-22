4d2f583e18585f671a3dae513a0dff35ff30395c
i8abyte <imransalim08@gmail.com>
2024-12-22 18:28:07 +1300

    fix: remove unused imports

--- src/tests/ChemicalElement.test.tsx
+++ src/tests/ChemicalElement.test.tsx
@@ -1,39 +1,38 @@
-import { describe, expect, it, test } from 'vitest'
-import { render, screen, fireEvent } from "@testing-library/react";
-import { useState } from "react";
+import { describe, expect, it } from 'vitest'
+import { render, screen } from "@testing-library/react";
 
 import { ChemicalElement } from '../stories/ChemicalElement';
 
-interface ChemicalElementProps {
-    /**
-     * What is the name of this element?
-     */
-    name?: string;
-    /**
-     * What is the symbol of this element?
-     */
-    symbol?: string;
-    /**
-     * What is the atomic number of this element?
-     */
-    atomicNumber?: number;
-    /**
-     * What row does this element belong to?
-     */
-    period?: number;
-    /**
-     * What column does this element belong to?
-     */
-    group?: number;
-    /**
-     * What is the chemical group block of this element?
-     */
-    backgroundColor?: string;
-     /**
-     * Optional click handler
-     */
-    onClick?: () => void;
-}
+// interface ChemicalElementProps {
+//     /**
+//      * What is the name of this element?
+//      */
+//     name?: string;
+//     /**
+//      * What is the symbol of this element?
+//      */
+//     symbol?: string;
+//     /**
+//      * What is the atomic number of this element?
+//      */
+//     atomicNumber?: number;
+//     /**
+//      * What row does this element belong to?
+//      */
+//     period?: number;
+//     /**
+//      * What column does this element belong to?
+//      */
+//     group?: number;
+//     /**
+//      * What is the chemical group block of this element?
+//      */
+//     backgroundColor?: string;
+//      /**
+//      * Optional click handler
+//      */
+//     onClick?: () => void;
+// }
 
 describe("ChemicalElement", () => {
     it("renders", () => {
