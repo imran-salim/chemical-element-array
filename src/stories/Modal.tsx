d67496e8e3c987da896e2be4265bbff0b049aeb2
i8abyte <imransalim08@gmail.com>
2024-12-26 18:06:10 +1300

    feat: add facts to be displayed in the modal to all elements

--- src/stories/Modal.tsx
+++ src/stories/Modal.tsx
@@ -4,9 +4,13 @@ interface ModalProps {
     atomicNumber: number;
     symbol: string;
     name: string;
+    period: number;
+    group: number;
+    atomicMass: number;
+    standardState: string;
 }
 
-export const Modal = ({ atomicNumber, symbol, name }: ModalProps) => {
+export const Modal = ({ atomicNumber, symbol, name, period, group, atomicMass, standardState }: ModalProps) => {
     return (
         <div className="modal-overlay">
             <div className="modal-container">
@@ -14,7 +18,11 @@ export const Modal = ({ atomicNumber, symbol, name }: ModalProps) => {
                 <div className="modal-content">
                     <h1>{name}</h1>
                     <h2>{symbol}</h2>
-                    <h4>{atomicNumber}</h4>
+                    <p>Atomic Number: {atomicNumber}</p>
+                    <p>Period: {period}</p>
+                    <p>Group: {group}</p>
+                    <p>Atomic Mass: {atomicMass} u</p>
+                    <p>Standard State: {standardState}</p>
                 </div>
             </div>
         </div>
